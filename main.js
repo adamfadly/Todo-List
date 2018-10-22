48ad6b9b190fc587e290a2c959c047de5fa759a4
adam fadly <adamfadly11@gmail.com>
2018-10-22 22:16:49 +0700

    To-do list update with css

--- main.js
+++ main.js
@@ -1,23 +1,26 @@
 // check off specific Todo by clicking
-$("ul").on("click", "li", function(){
+$("ul").on("click", "li","i", function(){
     $(this).toggleClass("completed");
 });
 
 // click on X to delete Todo
-$("ul").on("click", "span", function(event){
+$("ul").on("click", "span","i", function(event){
     $(this).parent().fadeOut(500,function(){
         $(this).remove();
     });
     event.stopPropagation();
 });
 
-$("input[type='text'").keyup(function(event){
+$("input[type='text']").keyup(function(event){
     if(event.which === 13){
      // garbing new Todo  text from input
      var todoText = $(this).val();
-     // create new li and add to ul
-     $("ul").append("<li><span> X </span>" + todoText + "<li>")
      $(this).val("");
-
+     // create new li and add to ul
+     $("ul").append("<li><i class='far fa-trash-alt'></i>" +" "+ todoText + "<li>")
     }
+});
+
+$(".fa-edit").click(function(){
+   $("input[type='text']").fadeToggle()
 });
