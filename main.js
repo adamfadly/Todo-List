28441f545ceab98089d79981aef3c73b68a5b981
adam fadly <adamfadly11@gmail.com>
2018-10-23 10:01:21 +0700

    finnished To-do list

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // check off specific Todo by clicking
-$("ul").on("click", "li","i", function(){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
 });
 
 // click on X to delete Todo
-$("ul").on("click", "span","i", function(event){
+$("ul").on("click", "span", function(event){
     $(this).parent().fadeOut(500,function(){
         $(this).remove();
     });
@@ -17,7 +17,7 @@ $("input[type='text']").keyup(function(event){
      var todoText = $(this).val();
      $(this).val("");
      // create new li and add to ul
-     $("ul").append("<li><i class='far fa-trash-alt'></i>" +" "+ todoText + "<li>")
+     $("ul").append("<li><span><i class='far fa-trash-alt'></i></span>" +" "+ todoText + "</li>")
     }
 });
 
